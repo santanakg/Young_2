c76c07cd785c0c1b46544d3ae272beaecee4586b
le0okk <140891498+le0okk@users.noreply.github.com>
2023-11-25 09:53:12 -0300

    finalizando quiz
    quiz maneiro

--- Aula_13/script.js
+++ Aula_13/script.js
@@ -32,18 +32,59 @@ const ALTERNATIVA_C = document.getElementById('texto-c')
 const ALTERNATIVA_D = document.getElementById('texto-d')
 const BOTAO = document.getElementById('enviar')
 const RESPOSTAS = document.querySelectorAll('.resposta')
-const CONTEINER_QUIZ = document.querySelector('conteiner-quiz')
+const CONTEINER_QUIZ = document.querySelector('.conteiner-quiz')
 
 let questao_atual = 0
 let qtd_corretas = 0
 
 function carregar_questoes(){
+    desmarcar()
     const QUESTAO = QUESTOES_QUIZ[questao_atual]
     TITULO_QUIZ.innerHTML = QUESTAO.questao
     ALTERNATIVA_A.innerHTML = QUESTAO.a
     ALTERNATIVA_B.innerHTML = QUESTAO.b
     ALTERNATIVA_C.innerHTML = QUESTAO.c
-    ALTERNATIVA_D.innerHTML = QUESTAO.d
+    ALTERNATIVA_D.innerHTML = QUESTAO.D
 }
+
+function desmarcar() {
+    RESPOSTAS.forEach((resp_elemento) => {
+        resp_elemento.checked = false
+    })
+}
+
+function pegar_resposta() {
+    let resp = undefined
+
+    RESPOSTAS.forEach((resposta) => {
+        if (resposta.checked) {
+            resp = resposta.id;
+        }
+    })
+
+    return resp
+}
+
+BOTAO.addEventListener('click', () => {
+    const RESP_PLAYER = pegar_resposta()
+    const TOTAL_PERGUNTAS = QUESTOES_QUIZ.length
+    console.log(RESP_PLAYER)
+
+    if (RESP_PLAYER) {
+        if (RESP_PLAYER === QUESTOES_QUIZ[questao_atual].correta){
+        qtd_corretas++
+       }
+        questao_atual++ 
+        if (questao_atual < TOTAL_PERGUNTAS){
+            carregar_questoes()
+
+        }
+        else {
+            CONTEINER_QUIZ.innerHTML = `<h1 class='resetar'>Você acertou ${qtd_corretas}/${TOTAL_PERGUNTAS} questoes </h1>
+            <button button class='resetar-btn' onclick='location.reload()'> Resetar </button>`
+        }
+        
+    }
+})
 
 carregar_questoes()
