af0c96ded9556eb729e3023e48eccf45d8149fb3
Weverthon-Miguel <136902870+santanakg@users.noreply.github.com>
2024-02-03 08:05:08 -0300

    Create Untitled-1.py

---
+++ Aula_15/Untitled-1.py
@@ -0,0 +1,23 @@
+#manipulando resultados e variaveis
+
+nome = input("Qual teu nome?")
+
+print("   \n ola"+nome)
+
+
+idade = input("Qual yua idade?")
+
+#dizendo a condição de poder estudar na ctrl play - imaginar que for por idade
+
+if idade <= 7:
+    print("Voce ainda não pode estudar conosco"+nome)
+
+else:
+    print("vem estudar conosco")    
+
+
+
+
+
+
+
